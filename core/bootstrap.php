b982b92faca854b07fa701d176abba56da1b1167
carOrLa03 <carmenorla@icloud.com>
2022-11-29 10:46:12 +0100

    mylog

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -3,8 +3,11 @@ require_once __DIR__ . "/../database/conexion.php";
 require_once __DIR__ . "/App.php";
 require_once __DIR__ . "/Request.php";
 require_once __DIR__ . "/Router.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 $config = require_once __DIR__ . "/../app/config.php";
+$logger = MyLog::load('logs/biblioteca.log');
 
 App::bind('config', $config);
+App::bind('logger', $logger);
 $conexion = App::getConexion();
