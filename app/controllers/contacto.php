e439e5eb7cc1aec8fffaad23662fdffc113b5e2e
carOrLa03 <carmenorla@icloud.com>
2022-12-13 09:29:03 +0100

    mail

--- app/controllers/contacto.php
+++ app/controllers/contacto.php
@@ -1,11 +1,14 @@
 <?php
 
 use biblioteca\App\entity\Mensajes;
-use bibliotecaCarmenDWS\App\repository\MensajesRepository;
+use biblioteca\App\exceptions\AppException;
+use biblioteca\App\exceptions\DataBaseException;
+use biblioteca\App\repository\MensajesRepository;
 use biblioteca\Core\App;
 
 require_once __DIR__ . "/../views/partials/menu.part.php";
 require_once __DIR__ . "/../../core/bootstrap.php";
+require_once './vendor/autoload.php';
 
 $nombre = "";
 $mail = "";
@@ -15,11 +18,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombre = htmlspecialchars(trim($_POST['nombre']));
     $mail = htmlspecialchars(trim($_POST['email']));
     $textarea = htmlspecialchars($_POST['textArea']);
-    if ((!preg_match("/^[a-zA-Z]+/", $nombre) || strlen($nombre) < 7)) {
+    if((!preg_match("/^[a-zA-Z]+/", $nombre) || strlen($nombre) < 7)) {
         $error = "El nombre no es correcto";
-    } else if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+    } else if(!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
         $error = "El email introducido no es correcto.";
-    } else if (strlen($textarea) < 10) {
+    } else if(strlen($textarea) < 10) {
         $error = "El comentario no debe estar vacío";
     } else {
         try {
@@ -27,24 +30,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mensajeNuevo = new MensajesRepository();
             $mensaje = new Mensajes($nombre, $mail, $textarea);
             $mensajeNuevo->save($mensaje);
-        } catch (\biblioteca\App\exceptions\DataBaseException $e) {
+        } catch (DataBaseException $e) {
         }
     }
-    if ($error != "ok") {
+    if($error != "ok") {
         try {
             App::get('logger')->add($error);
             echo "<div class='alert alert-danger' role='alert'>
             $error 
             </div>";
-        } catch (\biblioteca\App\exceptions\AppException $e) {
+        } catch (AppException $e) {
         }
     } else {
         $mensaje = " Tus datos han sido enviados correctamente.";
         echo "<div class='alert alert-success' role='alert'>
          $mensaje 
         </div>";
+
+        try {
+            // Create the Transport
+            $transport = (new Swift_SmtpTransport('smtp.gmail.com', 587, "tls"))->setUsername('carmenorla03@gmail.com')->setPassword('ColEbc210');
+            // Create the Mailer using your created Transport
+            $mailer = new Swift_Mailer($transport);
+            // Create a message
+            $message = (new Swift_Message('Bienvenida'))
+                ->setFrom(['carmenorla03@gmail.com' => 'Carmen'])
+                ->addTo($mail, $nombre)
+                ->setBody('Te damos la bienvenida a nuestra Biblioteca Online. La más grande de Internet.');
+            // Send the message
+            $result = $mailer->send($message);
+
+        } catch (Exception $e) {
+            $mensaje = $e->getMessage();
+            echo "<div class='alert alert-danger' role='alert'>
+         $mensaje 
+        </div>";
+        }
+
     }
 }
-
 
 require_once __DIR__ . "/../views/contacto.view.php";
